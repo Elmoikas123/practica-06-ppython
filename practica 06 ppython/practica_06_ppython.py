962e828c2ccd2a8c23da76746af947530313e1e2
mollo <mollo@PCMOIKAS>
2023-10-08 21:37:22 -0600

    Agregar archivos de proyecto.

---
+++ practica 06 ppython/practica_06_ppython.py
@@ -0,0 +1,18 @@
+# Declara una lista de cadenas
+aves = ["Loro gris", "Paloma de diamante", "Coctel"]
+print("Los valores de la lista antes de insertar:")
+# Itera sobre la lista para imprimir los valores
+for ave in aves:
+    print(ave, end=" ")
+
+print("\n")
+
+# Agrega 3 valores al final de la lista utilizando el método append()
+aves.append("Mayna")
+aves.append("Periquitos")
+aves.append("Cacatua")
+
+print("Los valores de la lista después de insertar:")
+# Itera sobre la lista para imprimir los valores
+for ave in aves:
+    print(ave, end=" ")
